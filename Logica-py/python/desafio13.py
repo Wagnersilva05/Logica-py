27d5859d10160b53b4c9b25a9b747fff16bca052
Wagnersilva05 <156625511+Wagnersilva05@users.noreply.github.com>
2024-02-26 18:40:07 -0300

    Add files via upload

---
+++ Logica-py/python/desafio13.py
@@ -0,0 +1,6 @@
+# Desafio do aumento de 15% do salário
+
+n1 = float(input('digite o salário:'))
+aum = (n1*15)/100 + n1
+
+print(f'Com o aumento de 15%, seu salário ficou R${aum}.')
---
+++ Logica-py/python/desafio8.py
@@ -0,0 +1,6 @@
+# Desafio da conversão dos metros
+n1 = float(input('Digite um valor'))
+cm = n1 * 100
+ml = n1 * 1000
+
+print(f'O valor convertido em centímetros é {cm}, e em milímetros é {ml}')
